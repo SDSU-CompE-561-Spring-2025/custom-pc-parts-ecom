905c25ba27e5e16fedbc4a3b07a23df959d862b9
DianaYousefnejad <126136334+DianaYousefnejad@users.noreply.github.com>
2025-04-13 20:44:19 -0700

    comments + login fixes

--- Backend/backend-implementation/app/auth.py
+++ Backend/backend-implementation/app/auth.py
@@ -11,20 +11,26 @@ from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.database import get_db
 from app.password_utils import get_password_hash, verify_password
+from dotenv import load_dotenv
+# loads the environment variables from the .env file
+load_dotenv()
 # Security configs 
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
+#password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+#compares and verifies the plain password against its hashed password version
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+#verifies username and password to authenticate the user
 def authenticate_user(db: Session, username: str, password: str):
     user = crud.get_user_by_username(db, username)
     if not user:
@@ -33,6 +39,7 @@ def authenticate_user(db: Session, username: str, password: str):
         return False
     return user
 
+#creates JWT access token
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
@@ -43,6 +50,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+#uses the  JWT access token to get the current user
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -62,6 +70,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         raise credentials_exception
     return user
 
+#checks if the current user is active
 async def get_current_active_user(current_user: models.User = Depends(get_current_user)):
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")

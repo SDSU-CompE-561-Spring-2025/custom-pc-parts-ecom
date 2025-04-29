fd4e433506b6a268465987bee9ad90aeb40b4293
Yousif287 <yfaraj0454@sdsu.edu>
2025-04-29 14:17:55 -0700

    Updated homepage and related component

--- Frontend/Frontend Implementation/next.config.js
+++ Frontend/Frontend Implementation/next.config.js
@@ -1,5 +1,25 @@
-module.exports = {
-    images: {
-      remotePatterns: [new URL('https://upload.wikimedia.org/wikipedia/commons/7/78/Pc_builder_logo.jpg')],
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.nvidia.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tpucdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'c1.neweggimages.com',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig

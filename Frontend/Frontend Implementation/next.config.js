7b52914c4dcf5f79f170f9ea9b1e4f3165678e16
Martina Azabo <martinaazabo@gmail.com>
2025-05-03 19:13:03 -0700

    category page created

--- Frontend/Frontend Implementation/next.config.js
+++ Frontend/Frontend Implementation/next.config.js
@@ -1,22 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
       },
       {
-        protocol: 'https',
-        hostname: 'www.nvidia.com',
+        protocol: "https",
+        hostname: "www.nvidia.com",
       },
       {
-        protocol: 'https',
-        hostname: 'tpucdn.com',
+        protocol: "https",
+        hostname: "tpucdn.com",
       },
       {
-        protocol: 'https',
-        hostname: 'c1.neweggimages.com',
+        protocol: "https",
+        hostname: "c1.neweggimages.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
       },
     ],
   },

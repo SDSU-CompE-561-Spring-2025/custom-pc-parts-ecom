da352a715aa795160d4be6ccf92583d7645cc53b
hhsw0322 <suwaix22@gmail.com>
2025-05-05 15:17:22 -0700

    Update next.config.js to allow CDN from all corresponding websites to images

--- Frontend/Frontend Implementation/next.config.js
+++ Frontend/Frontend Implementation/next.config.js
@@ -37,6 +37,54 @@ const nextConfig = {
         protocol: "https",
         hostname: "m.media-amazon.com",
       },
+      {
+        protocol: "https",
+        hostname: "assets.corsair.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.datocms-assets.com",
+      },
+      {
+        protocol: "https",
+        hostname: "c1.neweggimages.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.lg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.masterfolo.lv",
+      },
+      {
+        protocol: "https",
+        hostname: "dlcdnwebimgs.asus.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.aoc.com",
+      },
+      {
+        protocol: "https",
+        hostname: "asset.msi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.croma.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage-asset.msi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "asset-us-store.msi.com",
+      },
+      {
+        protocol: "https",
+        hostname: "altex.com",
+      },
     ],
   },
 }
